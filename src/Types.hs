166dd5dc1a2b88b3d160e3ff7a60a4f75d99b7ef
Patrick <Patrick@nytyler.com>
2024-11-07 10:44:04 -0500

    added some types for move

--- src/Types.hs
+++ src/Types.hs
@@ -1,16 +1,25 @@
 type Position = Int
 data Move = Left | Right | Back
 
+data Orientation = Position deriving (Eq) 
+
 
 data GameState = GameState {
     playerPosition    :: Position,
     batPosition       :: Position,
     pitPosition       :: Position,
     wumpusPosition    :: Position,
+    orientation       :: Orientation,
     wumpusIsHungry    :: Bool,
     playerArrowCount  :: Int
 }
 
 
-move :: Position -> Move -> Move
-move = undefined
+move :: Position -> Orientation -> Move -> Position
+-- move 1 2 Main.Left = 8
+-- move 1 8 Main.Left = 8
+-- move 1 5 Main.Left = 8
+
+move _ _ _ = undefined
+
+

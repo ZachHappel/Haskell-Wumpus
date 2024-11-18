8c3027ea011ff3243847e6f5b35c17d7aca61f30
Daniel T.  Mathew <danielt.mathew@148-100-161-108.foxnet.marist.edu>
2024-11-18 11:46:35 -0500

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,28 @@ type GameState = (PlayerState, WumpusState, EnvironmentState, gen)
 
 f :: CaveLayout -> GameState -> GameState
 f = undefined
+move :: CaveLayout -> Position -> Position -> Move -> Position
+move layout currentPosition lastPosition moveType =
+  case lookup currentPosition layout of
+    Just connections ->
+      case moveType of
+        Back -> lastPosition
+        Left -> cyclicMove connections lastIndex (-1)
+        Right -> cyclicMove connections lastIndex 1
+    Nothing -> error "Invalid position in cave layout"
+  where
+    -- Index of the last position in the connections list
+    lastIndex = case elemIndex lastPosition connections of
+                  Just idx -> idx
+                  Nothing -> error "Last position not found in connections"
 
+    -- Helper function for cyclic movement
+    cyclicMove :: [Position] -> Int -> Int -> Position
+    cyclicMove conns currentIndex step =
+      let newIndex = (currentIndex + step) `mod` length conns
+      in conns !! newIndex
+      
 x = do
   msg <- GameState
   putStrLn msg
+
